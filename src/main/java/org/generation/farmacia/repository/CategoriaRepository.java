89a4f69e80c9c51b63ff3f8b1bed950262e2e10a
Giyo Dias <112238210+giyodias@users.noreply.github.com>
2023-03-25 15:44:01 -0300

    Update CategoriaRepository.java

--- src/main/java/org/generation/farmacia/repository/CategoriaRepository.java
+++ src/main/java/org/generation/farmacia/repository/CategoriaRepository.java
@@ -5,6 +5,7 @@ import org.generation.farmacia.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
 	
